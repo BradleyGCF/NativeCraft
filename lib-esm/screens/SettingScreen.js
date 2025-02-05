6b2300c8023870df01d342c45a44400c76c45d16
Valenthy24rg <valenthy24rg@gmail.com>
2025-02-05 15:03:39 -0500

    Se eliminaron y modificaron archivos que crean confilicto al empaquetar los componentes

--- lib-esm/screens/SettingScreen.js
+++
@@ -1,9 +0,0 @@
-import { View } from "react-native";
-import { SwitchComponent } from "../modules/library/components/Switch/Switch";
-import { TextInputComponent } from "../modules/library/components/Input/Input";
-export const SettingScreen = () => {
-    return (<View style={{ flex: 1, justifyContent: "center" }}>
-      <SwitchComponent />
-      <TextInputComponent text="Enter a text"/>
-    </View>);
-};
