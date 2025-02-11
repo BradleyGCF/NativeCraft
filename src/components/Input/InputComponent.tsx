476a526bd630221825cd2689a9d77a38c041f035
Valenthy24rg <valenthy24rg@gmail.com>
2025-02-11 18:05:28 -0500

    Empaqutar los demás componentes

---
+++ src/components/Input/InputComponent.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+
+interface InputProps {
+  text: string;
+}
+
+export const InputComponent: React.FC<InputProps> = ({ text }) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder={text}
+        keyboardType="default"
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    width: 120,
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+  },
+  container: {
+    flex: 1,
+    marginTop: "-90%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { ButtonComponent } from "./components/ButtonComponent";
-// export { ModalComponent } from './components/Modal/Modal';
-// export { SwitchComponent } from './components/Switch/Switch';
-// export { TextInputComponent } from "./components/Input/Input";
+export { ModalComponent } from "./components/Modal/ModalComponent";
+export { SwitchComponent } from './components/Switch/SwitchComponent';
+export { InputComponent } from "./components/Input/InputComponent";
 
